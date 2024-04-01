24c4e6393639fda4148ad0d114c261bf16da969a
Aleksey Vetrov <vetrovalexey28@gmail.com>
2024-04-01 20:13:47 +0300

    обавил дополнительные тесты должен построиться успешный билд

--- src/test/java/ananasovitch/org/act/BoundaryServiceTest.java
+++ src/test/java/ananasovitch/org/act/BoundaryServiceTest.java
@@ -1,13 +1,13 @@
 package ananasovitch.org.act;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class BoundaryServiceTest {
 
@@ -18,6 +18,7 @@ public class BoundaryServiceTest {
                 arguments(new int[]{1, 2, 3, 4, 5}, 1),
                 arguments(new int[]{10, 20, 30, 40, 50}, 10),
                 arguments(new int[]{100, 200, 300, 400, 500}, 100)
+                // Добавьте еще тестовых данных здесь
         );
     }
 
@@ -26,5 +27,20 @@ public class BoundaryServiceTest {
     public void testFindMin(int[] marks, int expectedMin) {
         int min = boundaryService.findMin(marks);
         assertEquals(expectedMin, min, "Минимальное значение неверно");
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideAdditionalData")
+    public void testFindMinAdditional(int[] marks, int expectedMin) {
+        int min = boundaryService.findMin(marks);
+        assertEquals(expectedMin, min, "Минимальное значение неверно");
+    }
+
+    static Stream<Arguments> provideAdditionalData() {
+        return Stream.of(
+                arguments(new int[]{100, 5, 300, 20, 500}, 5),
+                arguments(new int[]{-10, -20, -3, -40, -5}, -40),
+                arguments(new int[]{10, 20, 30, 40, 5}, 5)
+        );
     }
 }
